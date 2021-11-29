3a6efc1ebc702fdebabca3b3d97ea2828dbdfb50
mathew-success <mathew@success.app>
2021-11-29 18:22:00 +0530

    Personal information and Reverse triangle task completed

---
+++ personal_info.php
@@ -0,0 +1,23 @@
+<?php
+
+$personal = [
+	'name' => 'Retson Mathew',
+	'email' => 'retson@success.app',
+	'address1' => [
+		'Door no' => '25B',
+		'Street Name' => 'Thillainagar 11 th cross',
+        'Area' => 'Thillainagar',
+        'City' => 'Trichy'
+	],
+	'address2' => [
+		'Door no' => '346',
+		'Street Name' => 'Thillainagar first cross',
+        'Area' => 'Thillainagar',
+        'City' => 'Trichy'
+	],
+	'phone_numbers' => [9876543210, 8976785432]
+];
+
+print_r($personal);
+
+?>
